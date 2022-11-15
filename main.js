42c20960da4b6d5cb9b7daf607a5da680ab770c8
Adam Lu <adamlu09@gmail.com>
2022-11-14 21:22:27 -0800

    Final commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // STATE
 let state = {
     board: [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
     ],
     players: ['X', 'O'],
     currentPlayer: 0
@@ -18,15 +18,6 @@ const submitEl = document.querySelector(".submitButton");
 const playerOneOutput = document.querySelector(".playerOneOutput");
 const playerTwoOutput = document.querySelector(".playerTwoOutput");
 const boardEl = document.querySelector(".gameBoard");
-const cellOne = document.getElementById("0-0");
-const cellTwo = document.getElementById("0-1");
-const cellThree = document.getElementById("0-2");
-const cellFour = document.getElementById("1-0");
-const cellFive = document.getElementById("1-1");
-const cellSix = document.getElementById("1-2");
-const cellSeven = document.getElementById("2-0");
-const cellEight = document.getElementById("2-1");
-const cellNine = document.getElementById("2-2");
 const wrapperEl = document.querySelector(".board")
 const resetEl = document.querySelector(".reset");
 
@@ -43,7 +34,7 @@ playerOptionsEl.addEventListener("click", function() {
 function pickName() {
     // Will randomize who is X and who is O
     const randomNumber = Math.floor(Math.random() * 2);
-    if (randomNumber == 0) {
+    if(randomNumber == 0) {
         playerOneOutput.textContent = playerOneName.value + " - X";
         playerTwoOutput.textContent = playerTwoName.value + " - O"; 
     } else {
@@ -62,7 +53,7 @@ function renderGame() {
     // Clears board, so that it doesn't generate another board after clicking
     boardEl.textContent = "";
     state.board.forEach((row, rowIndex) => {
-        for ( let i = 0; i < row.length; i++) {
+        for( let i = 0; i < row.length; i++) {
             createCell(rowIndex, i);
         }
     })
@@ -72,59 +63,76 @@ renderGame();
 // Function will create cells and add a dataset for each one indicating which row and column the cell is in
 function createCell(rowIndex, columnIndex) {
     const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.dataset.row = rowIndex;
-            cell.dataset.column = columnIndex;
-            cell.textContent = state.board[rowIndex][columnIndex];
-            cell.id = rowIndex + "-" + columnIndex;
-            boardEl.append(cell);
+    cell.classList.add("cell");
+    cell.dataset.row = rowIndex;
+    cell.dataset.column = columnIndex;
+    cell.textContent = state.board[rowIndex][columnIndex];
+    boardEl.append(cell);
 }
 
 // Allows you to click a cell
 boardEl.addEventListener("click", function(event) {
-    if (event.target.matches(".cell")) {
+    if(event.target.matches(".cell")) {
         const row = event.target.dataset.row;
         const column = event.target.dataset.column;
         // Prevents users from selecting the same cell twice
-        if (state.board[row][column]) {
+        if(state.board[row][column]) {
             alert("Illegal Move");
         } else {
             // Will provide the content you want generated when you click a cell from the state object
             state.board[row][column] = state.players[state.currentPlayer];
             changeTurn();
         }
+        // Function checks row for win
+        function checkRow() {
+            if(state.board[row][0] === state.board[row][1] && state.board[row][1] === state.board[row][2]) {
+                alert("Winner! Game Over!");
+            }
+        }
+        // Function checks column for win
+        function checkCol() {
+            if(state.board[0][column] === state.board[1][column] && state.board[1][column] === state.board[2][column]) {
+                alert("Winner! Game Over!");
+            }
+        }
+        // Function checks diagonals for win
+        function checkDiagonal() {
+            if(state.board[0][0] === state.board[1][1] && state.board[1][1] === state.board[2][2]) {
+                alert("Winner! Game Over!");
+            }
+            if(state.board[0][2] === state.board[1][1] && state.board[1][1] === state.board[2][0]) {
+                alert("Winner! Game Over!");
+            }
+        }
+        // Function checks for draw
+        // function checkDraw() {
+        //     if() {
+        //         alert("Draw! Game Over!");
+        //     }
+        // }
+        checkRow();
+        checkCol();
+        checkDiagonal();
+        // checkDraw();
     }
     renderGame();
 })
 
+
 // Function will change the player's turn
-function changeTurn () {
+function changeTurn() {
      // Ternary operator, is an alternative for if...else
         // If currentPlayer is 0, then change to 1, if not then 0
         state.currentPlayer = state.currentPlayer === 0 ? 1 : 0;
-}
-
-// Function checks if there was a win or not
-function checkWin() {
-    if (cellOne != "" && cellOne === cellTwo & cellTwo === cellThree) {
-        console.log("Hi");
-    }
-}
-checkWin();
-
-function checkDraw() {
-    if (cellOne != "" && cellOne === cellTwo && cellTwo === cellThree && cellThree === cellFour && cellFour === cellFive && cellFive === cellSix && cellSix === cellSeven && cellSeven === cellEight && cellEight === cellNine) {
-        console.log("Hi");
-    }
 }
 
 // Will reset the page, could not be done previously when state was a constant
 resetEl.addEventListener("click", function() {
     state = {
         board: [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""]
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
         ],
         players: ['X', 'O'],
         currentPlayer: 0
